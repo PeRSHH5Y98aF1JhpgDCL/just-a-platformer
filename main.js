72678b13c72496f60a9fbb6c44cb8a17a7606548
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-01-01 12:48:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -611,7 +611,7 @@ function adjustScreen() {
 	if (lvlx < 0) {
 		lvlx = Math.floor(window.innerWidth/2) - Math.floor(player.x+playerSize/2);
 		if (lvlx > 0) lvlx = 0;
-		if (lvlx < window.innerWidth - level.length*blockSize) lvlx = Math.floor(level.length*blockSize - window.innerWidth);
+		if (lvlx < window.innerWidth - level.length*blockSize) lvlx = Math.floor(window.innerWidth - level.length*blockSize);
 	}
 	let lvly = Math.floor((window.innerHeight - level[0].length*blockSize) / 2);
 	if (lvly < 0) {
